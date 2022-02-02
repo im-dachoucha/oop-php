c7bef1f201ab1b9a814ffe1de3124ef7ec9f6122
abderrahmane <abderrahamane7@gmail.com>
2022-02-02 14:21:38 +0100

    class test

--- index.php
+++ index.php
@@ -6,6 +6,18 @@ spl_autoload_register(function ($class) {
     require_once "$class.php";
 });
 try {
+    $p1 = new Personne("Fred", "1999-12-20", "male");
+
+    echo '<pre>';
+
+    // echo $etudiant->to_string();
+    // echo $p1->to_string();
+
+    var_dump($p1);
+
+    // var_dump($etudiant);
+
+    echo '</pre>';
 } catch (Exception $e) {
     echo '<pre>';
     // var_dump($e);
