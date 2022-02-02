ef892d19cff56efcc437b5833cf7a5118e02ba72
abderrahmane <abderrahamane7@gmail.com>
2022-02-02 14:23:55 +0100

    etudiant class add

---
+++ Etudiant.php
@@ -0,0 +1,57 @@
+<?php
+// require_once "./Personne.php";
+class Etudiant extends Personne
+{
+    // done properties
+    private $class, $date_integration, $notes;
+
+    // todo constructor
+
+    public function __construct(Personne $personne, $class, $date_integration)
+    {
+        parent::__construct($personne->get_nom(), $personne->get_date_naissance(), $personne->get_genre());
+        $this->class = $class;
+        $this->date_integration = $date_integration;
+    }
+    public function f($value)
+    {
+        $this->dd = $value;
+    }
+
+    // public static function avec_personne($personne, $class, $date_integration)
+    // {
+    //     parent::__construct($personne->nom, $personne->date_naissance, $personne->genre);
+    // }
+
+
+    // done getters
+    public function get_class()
+    {
+        return $this->class;
+    }
+    public function get_date_integration()
+    {
+        return date_format($this->date_integration, "Y-m-d");
+    }
+
+    // done setters
+    public function set_class($class)
+    {
+        $this->class = $class;
+    }
+    public function set_date_integration($date_integration)
+    {
+        $this->date_integration = date_create($date_integration);
+    }
+    public function set_notes(array $notes)
+    {
+        $this->notes = $notes;
+    }
+    // todo custom methods
+    public function calculer_moyenne()
+    {
+        foreach ($this->notes as $note) {
+            var_dump($note);
+        }
+    }
+}
